3c7aed2f1fa57c984ede539718a7a2afc038869d
Jon Clausen <jon_clausen@silowebworks.com>
2020-12-23 14:51:52 -0500

    Add the ability to export an entry as a JSON file

---
+++ resources/assets/js/components/entries/EntryExportButton.vue
@@ -0,0 +1,27 @@
+<template>
+	<button
+		@click="exportEntry"
+		class="text-gray-500 focus:outline-none float-right"
+		v-tooltip="'Download this log entry to a JSON file on your local machine'"
+	>
+		<fa-icon icon="download" size="xs" fixed-width/>
+	</button>
+</template>
+<script>
+	import FileSaver from "file-saver";
+	import { formatJSONRaw } from "@/util/udf";
+	export default {
+		props : {
+			entry : {
+				type : Object,
+				required : true
+			}
+		},
+		methods : {
+			exportEntry(){
+					var file = new Blob( [ formatJSONRaw( JSON.stringify( this.entry ) ) ], { type: "application/json" } );
+                    FileSaver.saveAs( file, `logstash-entry-${this.entry.id}.json` );
+			}
+		}
+	}
+</script>
