1779c88c2608af96e853b48491f153a31f14e67b
jclausen <jclausen@ortussolutions.com>
2023-03-04 10:00:07 -0600

    QA updates

--- resources/assets/js/views/ProjectSnapshot.vue
+++ resources/assets/js/views/ProjectSnapshot.vue
@@ -3,18 +3,18 @@
 		<h2 class="text-gray-600 text-xl font-medium pb-2">
 		{{ $t( "Project" ) }}: {{ project.name }}
 		</h2>
-		<h3 class="text-gray-400 text-xl pb-2 mb-7 mt-7  border-gray-300 border-b">{{ $t( "Applications" ) }}:</h3>
+		<h3 class="text-gray-400 text-lg pb-2 mb-7 mt-7  border-gray-300 border-b">{{ $t( "Applications" ) }}:</h3>
 		<template
 			v-for="(application, index) in project.applications"
 			:key="`application-${application}`"
 		>
-			<application-snapshot :application="application"></application-snapshot>
+			<application-snapshot heading-class="h4" heading-size="md" heading-color-class="text-teal-700" :application="application"></application-snapshot>
 		</template>
 
 		<template v-if="project.beats.length">
-			<h3 class="text-gray-400 text-xl font-medium pb-2 border-gray-300 border-b mt-7">{{ $t( "FileBeats" ) }}:</h3>
+			<h3 class="text-gray-400 text-lg font-medium pb-2 border-gray-300 border-b mt-7">{{ $t( "FileBeats" ) }}:</h3>
 			<template v-for="(application, index) in project.beats">
-				<h4 class="text-teal-400 text-md font-medium pb-2 border-gray-300 border-b">{{ application.toTitleCase() }}</h4>
+				<h4 class="text-teal-700 text-md font-medium pb-2 border-gray-300 border-b mt-5">{{ application.toTitleCase() }}</h4>
 				<beats-list
 					:initialFilters='{ "fields.application" : application, sortOrder : "@timestamp DESC" }'
 				></beats-list>
