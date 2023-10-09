713c7be97451ecae85b7d0826df5db11cdc0ad3b
Michael Born <8106227+michaelborn@users.noreply.github.com>
2023-10-09 10:32:48 -0400

    Fix z-index on left sidebar
    Elements with `transform` have their own z-index stacking context, hence we need to specify `z-index` on the transformed element to prevent it from rendering behind the log graph.

--- resources/assets/js/layouts/inc/Sidebar.vue
+++ resources/assets/js/layouts/inc/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex transform" :class="isOpen ? 'opacity-100' : 'opacity-0 hidden'">
+  <div class="flex transform z-30" :class="isOpen ? 'opacity-100' : 'opacity-0 hidden'">
     <div
       :class="isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
       class="fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-gray-800 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0"
